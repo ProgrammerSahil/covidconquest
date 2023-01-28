9fb19466299f8299af8aa234e94b9ce1fc3af672
ProgrammerSahil <74780160+ProgrammerSahil@users.noreply.github.com>
2023-01-28 11:38:11 +0530

    v1.0

---
+++ scroll.js
@@ -0,0 +1,73 @@
+
+$(document).ready(function(){
+  $("#button").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#button1").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#button2").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1200, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#button3").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#button4").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1300, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
